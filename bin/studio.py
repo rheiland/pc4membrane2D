5519cb67f3f2097e15c26d624649d9d68c5e09cf
Randy Heiland <heiland@indiana.edu>
2022-03-30 21:47:41 -0400

    QProcess

--- bin/studio.py
+++ bin/studio.py
@@ -21,6 +21,7 @@ from xml.dom import minidom
 
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QProcess
 
 from config_tab import Config
 from cell_def_tab import CellDef 
